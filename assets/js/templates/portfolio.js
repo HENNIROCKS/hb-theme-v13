27ca921be1b20e6fbec08a16ab6924d7ce4d88b4
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2025-10-31 21:09:35 +0100

    Fix load-more script

--- assets/js/templates/portfolio.js
+++ assets/js/templates/portfolio.js
@@ -3,17 +3,29 @@ const button = document.querySelector(".js-more");
 
 let page = parseInt(element.getAttribute("data-page"));
 
+// Initial verstecken wenn keine weiteren Seiten verfügbar sind
+if (!page) {
+  button.classList.add("hidden");
+}
+
 const fetchArticles = async () => {
+  if (!page) return; // Wenn keine weiteren Seiten verfügbar sind
+
   let url = `${window.location.href.split("#")[0]}.json/page:${page}`; // see info
   try {
     const response = await fetch(url);
     const { html, more } = await response.json();
-    button.hidden = !more;
-    element.insertAdjacentHTML("beforeend", html);
-    page++;
+
+    if (html) {
+      element.insertAdjacentHTML("beforeend", html);
+      element.setAttribute("data-page", more ? page + 1 : 0);
+      page = more ? page + 1 : 0;
+    }
+
+    button.classList.toggle("hidden", !more || !page);
   } catch (error) {
     console.log("Fetch error: ", error);
+    button.classList.add("hidden");
   }
 };
-
 button.addEventListener("click", fetchArticles);
