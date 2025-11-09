8436587ac8093cbb5a20779f56ca615d8ac0e0d5
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2025-11-09 10:49:24 +0100

    No change, edit whitespace

--- snippets/scrolltop.php
+++ snippets/scrolltop.php
@@ -7,7 +7,6 @@
 ?>
 
 <div class="scrolltop">
-
     <button aria-label="Nach oben scrollen" class="scrolltop__button" onclick="location.href='#top'" type="button">
 
         <span class="scrolltop__text">
