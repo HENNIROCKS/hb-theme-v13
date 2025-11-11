ffca5443f4582a42e1483d5e8e978f74f92a0ee2
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2025-11-11 21:32:28 +0100

    Just come housecleaning, no change

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ use Kirby\Cms\App as Kirby;
 use Hennirocks\Helpers;
 
 Kirby::plugin('hennirocks/hb-theme-v13', [
+    'blueprints' => Helpers::mapFiles(__DIR__, 'blueprints', ['yml']),
+    'snippets'   => Helpers::mapFiles(__DIR__, 'snippets',   ['php']),
+    'templates'  => Helpers::mapFiles(__DIR__, 'templates',  ['php']),
+
     'hooks' => [
         'system.loadPlugins:after' => function () {
             if (!kirby()->plugin('hennirocks/hb-commons')) {
@@ -17,7 +21,4 @@ Kirby::plugin('hennirocks/hb-theme-v13', [
             }
         }
     ],
-    'blueprints' => Helpers::mapFiles(__DIR__, 'blueprints', ['yml']),
-    'snippets'   => Helpers::mapFiles(__DIR__, 'snippets', ['php']),
-    'templates'  => Helpers::mapFiles(__DIR__, 'templates', ['php']),
 ]);
